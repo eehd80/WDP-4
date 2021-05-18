30ed9563db06922e14df674aae9682333e1b120f
eehd80 <eehd80yul@gmail.com>
2021-05-18 12:21:24 +0900

    Add files via upload

--- !setting/html-setting/dist/js/comm.js
+++ !setting/html-setting/dist/js/comm.js
@@ -1,4 +1,8 @@
-$(function(){
-
-    
-})
+$(document).ready(function(){
+    $('.slide-intro').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        // slidesToShow: 3,
+        // slidesToScroll: 1,
+    });
+});
